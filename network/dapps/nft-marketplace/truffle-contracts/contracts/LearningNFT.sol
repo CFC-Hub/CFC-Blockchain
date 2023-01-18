be7c76678c618f25dfa9b1ae188ac9c2f586a10e
Abhijith S <abheaks.007@gmail.com>
2023-01-18 10:37:50 +0530

    updated sol

--- network/dapps/nft-marketplace/truffle-contracts/contracts/LearningNFT.sol
+++ network/dapps/nft-marketplace/truffle-contracts/contracts/LearningNFT.sol
@@ -3,38 +3,56 @@ pragma solidity ^0.8.1;
 
 import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 
-contract LearningNFT is ERC721{
+contract LearningNFT is ERC721 {
     constructor() ERC721("Learn", "LRN") {}
 
-    struct BadgeDetail{
-        uint256 contentId;
-        uint256 startedTimestamp;
-        uint256 endTimestamp;
+    struct BadgeDetail {
+        string firstName;
+        string lastName;
+        string contentId;
+        uint256 submittedTimestamp;
     }
 
     mapping(uint256 => BadgeDetail) BadgeDetails;
-    uint256 public registeredBadgeCount = 0;
 
-    event createLearningNFTEvent( uint256 nftID, uint256 startedTimestamp, uint256 endTimestamp,uint256 recordedTimestamp);
+    event createLearningNFTEvent(
+        uint256 nftID,
+        string firstName,
+        string lastName,
+        string contentId,
+        uint256 submittedTimestamp,
+        uint256 recordedTime
+    );
 
-    function createLearningNFT(uint256 contentId,
-        uint256 startedTimestamp,
-        uint256 endTimestamp) public {
-            BadgeDetails[registeredBadgeCount]=BadgeDetail(contentId,startedTimestamp,endTimestamp);
-            _mint(msg.sender, registeredBadgeCount);
-            registeredBadgeCount += 1;
-            uint256 recordedTime=block.timestamp;
-            emit createLearningNFTEvent(registeredBadgeCount-1,startedTimestamp,endTimestamp,recordedTime);
+    function createLearningNFT(
+        uint256 nftID,
+        string memory firstName,
+        string memory lastName,
+        string memory contentId,
+        uint256 submittedTimestamp
+    ) public {
+        BadgeDetails[nftID] = BadgeDetail(
+            firstName,
+            lastName,
+            contentId,
+            submittedTimestamp
+        );
+        _mint(msg.sender, nftID);
 
-
+        uint256 recordedTime = block.timestamp;
+        emit createLearningNFTEvent(
+            nftID,
+            firstName,
+            lastName,
+            contentId,
+            submittedTimestamp,
+            recordedTime
+        );
     }
-    function getBadgeDetails(uint256 badgeId)
-        public
-        view
-        returns (BadgeDetail memory)
-    {
+
+    function getBadgeDetails(
+        uint256 badgeId
+    ) public view returns (BadgeDetail memory) {
         return BadgeDetails[badgeId];
     }
-
-    
 }
